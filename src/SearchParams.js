793be8f32e294ae16153ff33e8475c248271cacf
Eduar <educartoons@gmail.com>
2019-09-16 01:23:14 -0500

    Custom Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropDown from './useDropdown';
 
 const SearchParams = () => {
   // React Hook
 
   const [location, setLocation] = useState('Seattle, WA');
-  const [animal, setAnimal] = useState('dog');
-  const [breed, setBreed] = useState('breed');
+
   const [breeds, setBreeds] = useState([]);
+
+  const [animal, AnimalDropdown] = useDropDown('Animal', 'Dog', ANIMALS);
+  const [breed, BreedDropdown] = useDropDown('Breed', '', breeds);
 
   return (
     <div className="search-params">
@@ -22,40 +25,9 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal:
-          <select
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
-            name=""
-            id="animal"
-          >
-            <option value="">All</option>
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={e => setBreed(e.target.value)}
-            onBlur={e => setBreed(e.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>All</option>
-            {breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+
         <button>submit</button>
       </form>
     </div>
