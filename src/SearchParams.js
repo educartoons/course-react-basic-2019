d593fefe56b649234f21fcab7ca952a8177f289c
Eduar <educartoons@gmail.com>
2019-09-16 00:32:01 -0500

    Creating a Search Component

---
+++ src/SearchParams.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SearchParams = () => {
+  const location = 'Seattle, WA';
+
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location:
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            type="text"
+          />
+        </label>
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
