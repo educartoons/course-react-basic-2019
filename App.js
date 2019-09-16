22d758c8909ce862bd4585702c3171ac9f1ac9d0
Eduar <educartoons@gmail.com>
2019-09-15 22:35:47 -0500

    Passing in Components props

--- App.js
+++ App.js
@@ -1,31 +1,32 @@
-const Pet = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Luna'),
-    React.createElement('h2', {}, 'Dog'),
-    React.createElement('h2', {}, 'Hanavese')
-  ])
-}
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
 
 const App = () => {
   return React.createElement(
-    'div',
+    "div",
     {
-      id: 'something-important'
+      id: "something-important"
     },
     [
-      React.createElement(
-        'h1',
-        {},
-        'Adopt Me!'
-      ),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cocktail"
+      }),
+      React.createElement(Pet, { name: "Doing", animal: "Cat", breed: "Mixed" })
     ]
-  )
+  );
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
